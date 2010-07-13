2ab0b6ee96db2e1b071c228d5245d1ee85424df3
Torsten Raudssus <torsten@metaluna4.de>
2010-07-13 20:55:18 +0200

    First (already working) commit, concept 1:1 copied from Catalyst::Model::CouchDB

---
+++ lib/Catalyst/Helper/Model/MongoDB.pm
@@ -0,0 +1,114 @@
+package Catalyst::Helper::Model::MongoDB;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+=pod
+
+=head1 NAME
+
+Catalyst::Helper::Model::MongoDB - Helper for MongoDB models
+
+=head1 SYNOPSIS
+
+ script/myapp_create.pl model MyModel MongoDB uri
+
+=head1 DESCRIPTION
+
+Helper for the L<Catalyst> MongoDB model.
+
+=head1 USAGE
+
+=head1 METHODS
+
+=head2 mk_compclass
+
+Makes the model class.
+
+=head2 mk_comptest
+
+Makes tests.
+
+=cut
+
+sub mk_compclass {
+    my ($self, $helper) = @_;
+
+    $helper->render_file('modelclass', $helper->{file});
+    return 1;
+}
+
+sub mk_comptest {
+    my ($self, $helper) = @_;
+    $helper->render_file('modeltest', $helper->{test});
+}
+
+=pod
+
+=head1 AUTHOR
+
+Torsten Raudssus <torsten@raudssus.de>
+
+=head1 BUGS 
+
+Please report any bugs or feature requests to me on IRC Getty at irc.perl.org, or make a pull request
+at http://github.com/Getty/catalyst-model-mongodb
+
+=head1 COPYRIGHT & LICENSE 
+
+Copyright 2010 Torsten Raudssus, all rights reserved.
+
+This library is free software; you can redistribute it and/or modify it under the same terms as 
+Perl itself, either Perl version 5.8.8 or, at your option, any later version of Perl 5 you may 
+have available.
+
+=cut
+
+1;
+
+__DATA__
+
+=begin pod_to_ignore
+
+__modelclass__
+package [% class %];
+
+use strict;
+use warnings;
+use base 'Catalyst::Model::MongoDB';
+
+__PACKAGE__->config(
+);
+
+=head1 NAME
+
+[% class %] - MongoDB Catalyst model component
+
+=head1 SYNOPSIS
+
+See L<[% app %]>.
+
+=head1 DESCRIPTION
+
+MongoDB Catalyst model component.
+
+=head1 AUTHOR
+
+[% author %]
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
+__modeltest__
+use strict;
+use warnings;
+use Test::More tests => 2;
+
+use_ok('Catalyst::Test', '[% app %]');
+use_ok('[% class %]');
---
+++ lib/Catalyst/Model/MongoDB.pm
@@ -0,0 +1,97 @@
+package Catalyst::Model::MongoDB;
+use base qw/Catalyst::Model/;
+use strict;
+use warnings;
+
+use NEXT;
+use Carp qw(confess);
+use MongoDB;
+
+our $VERSION = '0.01';
+
+sub new {
+    my ($class, $c, $config) = @_;
+    my $self = $class->NEXT::new($c, $config);
+    $self->config($config);
+
+    my $conf = $self->config;
+    $self->{mongodb_connection} = MongoDB::Connection->new($conf);
+    $c->log->debug("MongoDB::Connection instantiated") if $c->debug;
+
+    return $self;
+}
+
+sub AUTOLOAD {
+    my ($self, @args) = @_;
+    our $AUTOLOAD;
+    return if $AUTOLOAD =~ /::DESTROY$/;
+
+    (my $meth = $AUTOLOAD) =~ s/^.*:://;
+
+    return $self->{mongodb_connection}->$meth(@args);
+}
+
+
+1;
+
+=pod
+
+=head1 NAME
+
+Catalyst::Model::MongoDB - MongoDB model class for Catalyst
+
+=head1 SYNOPSIS
+
+    # model
+    __PACKAGE__->config(
+ 		host => 'localhost',
+		port => 27017,       
+    );
+
+    # controller
+    sub foo : Local {
+        my ($self, $c) = @_;
+
+        eval {
+            my @docs = $c->model('MyData')->mydatabase->mycollection->find();
+        };
+        ...
+    }
+
+
+=head1 DESCRIPTION
+
+This model class exposes L<MongoDB::Connection> as a Catalyst model.
+
+=head1 CONFIGURATION
+
+You can pass the same configuration fields as when you make a new L<MongoDB::Connection>.
+
+=head1 METHODS
+
+=head2 MongoDB
+
+All the methods not handled locally are forwarded to L<MongoDB::Connection>.
+
+=head2 new
+
+Called from Catalyst.
+
+=head1 AUTHOR
+
+Torsten Raudssus <torsten@raudssus.de>
+
+=head1 BUGS 
+
+Please report any bugs or feature requests to me on IRC Getty at irc.perl.org, or make a pull request
+at http://github.com/Getty/catalyst-model-mongodb
+
+=head1 COPYRIGHT & LICENSE 
+
+Copyright 2010 Torsten Raudssus, all rights reserved.
+
+This library is free software; you can redistribute it and/or modify it under the same terms as 
+Perl itself, either Perl version 5.8.8 or, at your option, any later version of Perl 5 you may 
+have available.
+
+=cut
